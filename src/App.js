a6123ca80bc4adcbdc14f93a4ab3ca96a9500a74
Slavi Shtipkov <development@shtipkov.com>
2019-12-09 05:55:57 +0200

    Init commit for simple book list app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import BookContextProvider from './contexts/BookContext';
+import Navbar from './components/Navbar';
+import BookList from './components/BookList';
+import BookForm from './components/BookForm';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BookContextProvider>
+        <Navbar />
+        <BookList />
+        <BookForm />
+      </BookContextProvider>
     </div>
   );
-}
+};
 
 export default App;
